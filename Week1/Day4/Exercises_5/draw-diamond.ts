559ed5cb916fef6eca442e4d26f1e156705e9fb2
NickPappagiorgio <nemes1988@icloud.com>
2018-02-15 22:54:07 +0100

    draw-diamond.ts

--- Week1/Day4/Exercises_5/draw-diamond.ts
+++ Week1/Day4/Exercises_5/draw-diamond.ts
@@ -1,7 +1,8 @@
 'use strict';
 
 let lineCount: number = 7;
-
+let star: string = '*'
+let space: string = ' '
 // Write a program that draws a
 // diamond like this:
 //
@@ -14,3 +15,24 @@ let lineCount: number = 7;
 //    *
 //
 // The diamond should have as many lines as lineCount is
+
+for (let i: number = 1; i <= lineCount - 3; i++) {
+    space = ' ';
+    for (let j: number = 1; j <= (lineCount - 3 - i); j++){
+        space = space + ' ';
+    }
+    console.log(space + star);
+    star = '*' + star + '*';
+}
+
+for (let i: number = 1; i <= lineCount - 4; i++) {
+    space = ' ';
+    for (let j: number = 1; j <= (i); j++){
+        space = space + ' ';
+    }
+    star = '*';
+    for (let j: number = 1; j <= (lineCount - i - 4); j++){
+        star = '*' + star + '*';
+    }
+    console.log(space + star);
+}
