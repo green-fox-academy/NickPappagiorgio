6aa75b6d41a4d72ffcce66291d4f691d51f18a86
NickPappagiorgio <nemes1988@icloud.com>
2018-02-15 15:28:19 +0100

    cuboid.ts

--- Week1/Day4/Exercises_3/cuboid.ts
+++ Week1/Day4/Exercises_3/cuboid.ts
@@ -5,3 +5,10 @@
 //
 // Surface Area: 600
 // Volume: 1000
+
+let height: number = 50;
+let width: number = 666;
+let depth: number = 333;
+
+console.log('Surface Area: ' + height * depth + ' m2');
+console.log('Volume: ' + height * depth * width + ' m3');
