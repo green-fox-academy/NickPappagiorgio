7812959adac84849ef5e6ef478986586513d2587
NickPappagiorgio <nemes1988@icloud.com>
2018-02-15 15:53:33 +0100

    seconds-in-a-day.ts

--- Week1/Day4/Exercises_3/seconds-in-a-day.ts
+++ Week1/Day4/Exercises_3/seconds-in-a-day.ts
@@ -6,3 +6,15 @@ let currentSeconds: number = 42;
 
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
+
+let pastSeconds: number = (currentHours * 3600) + (currentMinutes * 60) + (currentSeconds);
+
+let remainingSeconds: number = (24 * 3600) - pastSeconds;
+
+
+console.log(pastSeconds);
+console.log(remainingSeconds);
+
+if (pastSeconds + remainingSeconds === 86400) {
+    console.log(true);
+}
