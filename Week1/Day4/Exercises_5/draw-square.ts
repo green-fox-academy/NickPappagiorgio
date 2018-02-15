b648925c8ed267aac9f1612e994041d81f33770d
NickPappagiorgio <nemes1988@icloud.com>
2018-02-16 00:49:17 +0100

    draw-square.ts

--- Week1/Day4/Exercises_5/draw-square.ts
+++ Week1/Day4/Exercises_5/draw-square.ts
@@ -1,7 +1,8 @@
 'use strict';
 
-let lineCount: number = 6;
-
+let lineCount: number = 5;
+let percent: string = '%%%%%';
+let space: string = '%' + '   ' + '%';
 // Write a program that draws a square like this:
 //
 // %%%%%
@@ -12,3 +13,14 @@ let lineCount: number = 6;
 // %%%%%
 //
 // The square should have as many lines as lineCount is
+
+for (let i: number = 1; i <= lineCount; i++) {
+    
+        if (i == 1) {
+            console.log(percent);
+        } else if (i > 1 && i < lineCount) {
+            console.log(space);
+        } else {
+            console.log(percent);
+        }
+}
