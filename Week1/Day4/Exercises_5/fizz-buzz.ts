de4377b2125980bb80a3e6b10c9d80e932b5cfb3
NickPappagiorgio <nemes1988@icloud.com>
2018-02-15 19:18:01 +0100

    fizz-buzz.ts

--- Week1/Day4/Exercises_5/fizz-buzz.ts
+++ Week1/Day4/Exercises_5/fizz-buzz.ts
@@ -4,3 +4,15 @@
 // But for multiples of three print “Fizz” instead of the number
 // and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+let i: number = 0;
+let Fizz: string = 'Fizz';
+
+while (i < 100) {
+    i++;
+    if (i % 3 == 0) {
+        console.log(Fizz);
+    } else {
+        console.log(i);
+    }
+}
