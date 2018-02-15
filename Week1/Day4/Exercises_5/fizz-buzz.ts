764c658ff449789227f515ab0f66a99f8a5129cc
NickPappagiorgio <nemes1988@icloud.com>
2018-02-15 22:27:42 +0100

    fizz-buzz.ts

--- Week1/Day4/Exercises_5/fizz-buzz.ts
+++ Week1/Day4/Exercises_5/fizz-buzz.ts
@@ -5,14 +5,20 @@
 // and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
 
-let i: number = 0;
+let a: number = 0;
 let Fizz: string = 'Fizz';
+let Buzz: string = 'Buzz';
+let FizzBuzz: string = 'FizzBuzz';
 
-while (i < 100) {
-    i++;
-    if (i % 3 == 0) {
-        console.log(Fizz);
+while (a < 100) {
+    a++;
+    if (a % 15 == 0) {
+        console.log(FizzBuzz);
+    } else if (a % 3 == 0) {
+        console.log(Fizz)
+    } else if (a % 5 == 0) {
+        console.log(Buzz)
     } else {
-        console.log(i);
+        console.log(a);
     }
 }
