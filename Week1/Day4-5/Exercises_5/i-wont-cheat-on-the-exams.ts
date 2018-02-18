1ab62213f2cce9f62e9572007c2d9879fa5183b3
NickPappagiorgio <nemes1988@icloud.com>
2018-02-18 15:14:40 +0100

    fixing

--- Week1/Day4-5/Exercises_5/i-wont-cheat-on-the-exams.ts
+++ Week1/Day4-5/Exercises_5/i-wont-cheat-on-the-exams.ts
@@ -3,8 +3,12 @@
 // Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
 
-let a: number = 0;
+/*let a: number = 0;
 while (a < 101) {
     console.log('I won\'t cheat on the exam!')
     a++;
+}
+*/
+for (let i: number = 1; i <= 100; i++) {
+    console.log(i + ' I won\'t cheat on the exam!');
 }
