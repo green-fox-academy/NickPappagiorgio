91a87ae9da77577922b6a28cf0db626fa6e94842
NickPappagiorgio <nemes1988@icloud.com>
2018-02-20 17:17:29 +0100

    W2D2

---
+++ Week2/Day2/Arrays/02_Matchmaking.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a function that joins two array by matching one girl with one boy in a new array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls: string[] = ['Eve', 'Ashley', 'Bözsi', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Béla', 'Todd', 'Neef', 'Jeff'];
+
+function mix(girlsArray: string[], boysArray: string[]) {
+  let mix: string [] = [];
+  for (let i: number = 0; i < girls.length; i++) {
+    mix = mix.concat(girls[i],boys[i]);
+  }
+  return mix.toString();
+}
+console.log(mix(girls,boys))
---
+++ Week2/Day2/Arrays/03_Append_letter.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ['kuty', 'macsk', 'kacs', 'rók', 'halacsk'];
+
+function appendA(newFar: string[]) {
+  newFar = newFar.map(newFarA => newFarA + 'a')
+  return newFar;
+}
+
+console.log(appendA(far));
+
+// The output should be: 'kutya', 'macska', 'kacsa', 'róka', 'halacska'
