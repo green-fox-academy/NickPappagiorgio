2e1c04fc039967eacdebd89abc37f15a049acbfb
NickPappagiorgio <nemes1988@icloud.com>
2018-02-20 15:31:14 +0100

    W2D2

--- Week2/Day2/Strings/02_Reverse.ts
+++ Week2/Day2/Strings/02_Reverse.ts
@@ -3,6 +3,8 @@
 // Create a function that can reverse a string, which is passed as the parameter
 // Use it on this reversed string to check it!
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
-console.log(reverse(reversed));
 
-export = reverse;
+
+reversed = reversed.split('').reverse().join('');
+
+console.log(reversed);
