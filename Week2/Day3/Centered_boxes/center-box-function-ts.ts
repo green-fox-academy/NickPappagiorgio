d8bd1defd23f185b949d06a5aa4f62c1549b7f40
NickPappagiorgio <nemes1988@icloud.com>
2018-02-21 15:29:08 +0100

    Week2Day3

---
+++ Week2/Day3/Centered_boxes/center-box-function-ts.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 1 parameter:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+let colors: string[] = ['blue', 'red', 'green']
+
+function square(x: number, color: string) {
+  ctx.fillStyle = color;
+  ctx.fillRect(300 - (x / 2), 200 - (x / 2), x, x);
+}
+
+let x: number = 400;
+
+colors.forEach(function(AColor){
+  square(x,AColor);
+  x -= 50;
+})
---
+++ Week2/Day3/Square_positioning/position-square-ts.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 2 parameters:
+// The x and y coordinates of the square's top left corner
+// and draws a 50x50 square from that point.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+
+function square(x: number, y: number) {
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(x,y,50,50)
+}
+  let x: number = 20;
+  let y: number = 20;
+
+  for (let i: number = 0; i < 3; i++) {
+    x = x + 60;
+    square(x,y);
+  }
