26c17756af648895f6f9dd78fc61a90abda6a108
NickPappagiorgio <nemes1988@icloud.com>
2018-02-21 15:39:30 +0100

    Week2Day3

--- Week2/Day3/Rainbow_boxes/rainbow-box-function-ts.ts
+++ Week2/Day3/Rainbow_boxes/rainbow-box-function-ts.ts
@@ -4,17 +4,17 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
-// Create a square drawing function that takes 1 parameter:
-// The square size
-// and draws a square of that size to the center of the canvas.
-// Draw 3 squares with that function.
-// Avoid code duplication.
+// Create a square drawing function that takes 2 parameters:
+// The square size, and the fill color,
+// and draws a square of that size and color to the center of the canvas.
+// Create a loop that fills the canvas with rainbow colored squares.
+
 
 let colors: string[] = ['blue', 'red', 'green']
 
 function square(x: number, color: string) {
-  ctx.strokeStyle = color;
-  ctx.strokeRect(300 - (x / 2), 200 - (x / 2), x, x);
+  ctx.fillStyle = color;
+  ctx.fillRect(300 - (x / 2), 200 - (x / 2), x, x);
 }
 
 let x: number = 400;
