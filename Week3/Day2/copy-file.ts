58607c2d1b68ad63a6d85b96717b528fc2c9177b
NickPappagiorgio <nemes1988@icloud.com>
2018-02-27 15:28:48 +0100

    Fucking egotistical bastard. adds expandtab to vimrc

---
+++ Week3/Day2/copy-file.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Write a function that copies a file to an other
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+const fs = require('fs');
+let fileOne: string = 'from.txt';
+let fileTwo: string = 'to.txt';
+
+
+function copy(from: string, to: string) {
+  try {
+    fs.writeFileSync(to, (fs.readFileSync(from, 'utf-8')));
+    return true;
+  }
+  catch (error) {
+    return false;
+  }
+} console.log(copy(fileOne, fileTwo));
+
+
