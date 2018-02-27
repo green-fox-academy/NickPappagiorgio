d52d1bf3af9a75157106e57cc7889dadce46b2c4
NickPappagiorgio <nemes1988@icloud.com>
2018-02-27 14:22:57 +0100

    Committing fixes in the dark, seriously, who killed my power!?

---
+++ Week3/Day2/write-multiple-lines.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is 'apple' and the number is 5, than it should write 5 lines
+// to the file and each line should be 'apple'
+// The function should not raise any error if it could not write the file.
+
+const fs = require('fs');
+let fileName: string = 'myfile2.txt';
+
+function writeIn(path: string, word: string, number: number) { fs.writeFileSync(path, (fs.readFileSync(path, 'utf-8')) + '\n' + word);
+
+}
+writeIn(fileName, 'XD', 5);
