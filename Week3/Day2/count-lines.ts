60770056a12059963727d2e7d39e42de6fc74091
NickPappagiorgio <nemes1988@icloud.com>
2018-02-27 13:09:50 +0100

    remove certain things and added stuff

---
+++ Week3/Day2/count-lines.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+function fileName(name: string) {
+  let array: any[] = name.split('\n');
+  return array.length;
+}
+
+try {
+  const fs = require('fs');
+  let fileContent = fs.readFileSync('myfile.txt', 'utf-8');
+  console.log(fileName(fileContent));
+}
+
+catch (error) {
+  console.log('Nyazsgem!');
+}
