ffed1542c5611e681f50bd3d8082906aec6ae878
NickPappagiorgio <nemes1988@icloud.com>
2018-02-28 11:47:57 +0100

    Friday 5pm

---
+++ Week3/Day3/post-it.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+class PostIt {
+  backgroundColor: string;
+  text: string;
+  textColor: string;
+
+  constructor(backgroundColorIn, textIn, textColorIn) {
+    this.backgroundColor = backgroundColorIn;
+    this.text = textIn;
+    this.textColor = textColorIn;
+  }
+}
+
+let orange = new PostIt ('orange', 'Idea1', 'blue');
+console.log(orange);
+
+let pink = new PostIt ('pink', 'Awesome', 'black');
+console.log(pink);
+
+let yellow = new PostIt ('yellow', 'Superb', 'green');
+console.log(yellow);
