1ddecdd808546509a134b57e3d651483512ea765
NickPappagiorgio <nemes1988@icloud.com>
2018-03-02 15:31:40 +0100

    Fixing

--- Week3/Day4/12-Draw/12-draw.ts
+++ Week3/Day4/12-Draw/12-draw.ts
@@ -13,15 +13,17 @@ function bullshit(x: number, y: number, z: number) {
   ctx.strokeRect(x + z, y, z, z);
   ctx.strokeRect(x, y + z, z, z);
 
-  if (z >= 10) {
-    ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-    bullshit(x - (z / 3) * 2, y + (z / 3), z / 3);
-    ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-    bullshit(x + (z / 3), y - (z / 3) * 2, z / 3);
-    ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-    bullshit(x + (z / 3) * 4, y + (z / 3), z / 3);
-    ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-    bullshit(x + (z / 3), y + (z / 3) * 4, z / 3);
+  if (z >= 1) {
+    setTimeout(function () {
+      ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+      bullshit(x - (z / 3) * 2, y + (z / 3), z / 3);
+      ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+      bullshit(x + (z / 3), y - (z / 3) * 2, z / 3);
+      ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+      bullshit(x + (z / 3) * 4, y + (z / 3), z / 3);
+      ctx.strokeStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+      bullshit(x + (z / 3), y + (z / 3) * 4, z / 3);
+    }, 600)
   }
 }
 bullshit(size, size, size);
