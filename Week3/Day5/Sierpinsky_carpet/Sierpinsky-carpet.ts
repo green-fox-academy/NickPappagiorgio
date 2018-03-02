20cf2fa505abdd145f44779f270a63fc9f222c68
NickPappagiorgio <nemes1988@icloud.com>
2018-03-02 15:43:44 +0100

    So hardcore-fixing

--- Week3/Day5/Sierpinsky_carpet/Sierpinsky-carpet.ts
+++ Week3/Day5/Sierpinsky_carpet/Sierpinsky-carpet.ts
@@ -8,14 +8,14 @@ ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
 let x: number = canvas.width / 3;
 let y: number = canvas.height / 3;
 let z: number = canvas.width / 3;
-let counter: number = 11;
+let counter: number = 7;
 
 
 function bullshit(x: number, y: number, z: number, c: number) {
 
   ctx.fillRect(x, y, z, z);
 
-  if (counter >= 1) {
+  if (c >= 1) {
     setTimeout(function () {
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
       bullshit(x - (z / 1.5), y - (z / 3) * 2, z / 3, c - 1); // TOP_LEFT
