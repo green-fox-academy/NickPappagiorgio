ba383bb54ca94c8c0442af07651a2a6b06bd86fa
NickPappagiorgio <nemes1988@icloud.com>
2018-03-03 08:41:10 +0100

    Fixing

--- Week3/Day5/Sierpinsky_carpet/Sierpinsky-carpet.ts
+++ Week3/Day5/Sierpinsky_carpet/Sierpinsky-carpet.ts
@@ -18,28 +18,21 @@ function bullshit(x: number, y: number, z: number, c: number) {
   if (c >= 1) {
     setTimeout(function () {
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x - (z / 1.5), y - (z / 3) * 2, z / 3, c - 1); // TOP_LEFT
-
+      bullshit(x - (z / 1.5), y - (z / 3) * 2, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x + (z / 3), y - (z / 3) * 2, z / 3, c - 1); // TOP
-
+      bullshit(x + (z / 3), y - (z / 3) * 2, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x + (z + z / 3), y - (z / 3) * 2, z / 3, c - 1); // TOP_RIGHT
-
+      bullshit(x + (z + z / 3), y - (z / 3) * 2, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x + (z / 3) * 4, y + (z / 3), z / 3, c - 1); // RIGHT
-
+      bullshit(x + (z / 3) * 4, y + (z / 3), z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x + (z + z / 3), y + (z / 3) * 4, z / 3, c - 1); //BOTTOM_RIGHT
-
+      bullshit(x + (z + z / 3), y + (z / 3) * 4, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x + (z / 3), y + (z / 3) * 4, z / 3, c - 1); // BOTTOM
-
+      bullshit(x + (z / 3), y + (z / 3) * 4, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x - (z / 1.5), y + (z / 3) * 4, z / 3, c - 1); //BOTTOM_LEFT
-
+      bullshit(x - (z / 1.5), y + (z / 3) * 4, z / 3, c - 1);
       ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
-      bullshit(x - (z / 3) * 2, y + (z / 3), z / 3, c - 1); // LEFT
+      bullshit(x - (z / 3) * 2, y + (z / 3), z / 3, c - 1);
     }, 600)
   }
 }
