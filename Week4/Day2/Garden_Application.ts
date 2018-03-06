f00b28d7cd27009f5864cb323236ee347cf4f070
NickPappagiorgio <nemes1988@icloud.com>
2018-03-06 18:45:47 +0100

    Just committing so I can go home

---
+++ Week4/Day2/Garden_Application.ts
@@ -0,0 +1,71 @@
+'use strict';
+
+class Garden {
+  plantsArray: Plant[] = [];
+  collect(plant: Plant) {
+    this.plantsArray.push(plant)
+  }
+  watering(amount: number) {
+    let newPlantsArray: Plant[] = [];
+    for (let i: number = 0; i < this.plantsArray.length; i++) {
+      if (this.plantsArray[i].level()) {
+        newPlantsArray.push(this.plantsArray[i]);
+      }
+    }
+    for (let i: number = 0; i < newPlantsArray.length; i++) {
+      newPlantsArray[i].watering(amount / newPlantsArray.length);
+    }
+  }
+}
+
+class Plant {
+  color: string;
+  type: string;
+  waterLevel: number = 0;
+
+  constructor(color: string) {
+    this.color = color;
+  }
+  level() { }
+  watering(amount: number) { }
+}
+
+class Flower extends Plant {
+  constructor(color: string) {
+    super(color);
+    this.type = 'flower';
+  }
+  level() {
+    return (this.waterLevel < 5);
+  }
+
+  watering(amount: number) {
+    this.waterLevel += (amount  *  0.75);
+  }
+}
+
+class Tree extends Plant {
+  constructor(color: string) {
+    super(color);
+    this.type = 'tree';
+  }
+  level() {
+    return (this.waterLevel < 10);
+  }
+
+  watering(amount: number) {
+    this.waterLevel += (amount  *  0.4);
+  }
+}
+
+let myGarden = new Garden();
+let yellow = new Flower('yellow');
+myGarden.collect(yellow)
+let blue = new Flower('blue');
+myGarden.collect(blue)
+let purple = new Tree('purple');
+myGarden.collect(purple)
+let orange = new Tree('orange');
+myGarden.collect(orange)
+//console.log(myGarden.plantsArray);
+console.log(myGarden.watering(40));
