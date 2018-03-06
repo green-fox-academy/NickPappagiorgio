e4c6cd731b8251acc34632faa17aca5eef2679d6
NickPappagiorgio <nemes1988@icloud.com>
2018-03-06 09:01:24 +0100

    Homework

---
+++ Week4/Day2/Homework.ts
@@ -0,0 +1,91 @@
+'use strict';
+
+class Person {
+  name: string;
+  age: number;
+  gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  introduce() {
+    return `Hi, I'm ${this.name}, a ${this.age} year old ${this.gender}.`;
+  }
+  getGoal() {
+    return "My goal is: Live for the moment!";
+  }
+}
+let person = new Person('Jane Doe', 30, 'female');
+
+class Student {
+  name: string;
+  age: number;
+  gender: string;
+  previousOrganization: string;
+  skippedDays: number = 0;
+
+  constructor(name: string, age: number, gender: string, previousOrganization: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+  }
+  introduce() {
+    return `Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already.`;
+  }
+  getGoal() {
+    return "Be a junior software developer.";
+  }
+  skipDays(numberOfDays) {
+    this.skippedDays += numberOfDays;
+  }
+}
+let student = new Student('Jane Doe', 30, 'female', 'The School of Life');
+
+class Mentor {
+  name: string;
+  age: number;
+  gender: string;
+  level: string;
+
+  constructor(name: string, age: number, gender: string, level: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+  introduce() {
+    return `Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`;
+  }
+  getGoal() {
+    return "Educate brilliant junior software developers.";
+  }
+}
+let mentor = new Mentor('Jane Doe', 30, 'femail', 'intermediate');
+
+class Sponsor {
+  name: string;
+  age: number;
+  gender: string;
+  company: string;
+  hiredStudents: number;
+
+  constructor(name: string, age: number, gender: string, company: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.company = company;
+  }
+  introduce() {
+    return `Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`;
+  }
+  hire() {
+    this.hiredStudents++;
+  }
+  getGoal() {
+    return "Hire brilliant junior software developers.";
+  }
+}
+let sponsor = new Sponsor('Jane Doe', 30, 'female', 'Google');
