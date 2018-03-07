af7b9eea76ac0ec41488e3fabf274f8796d2f5ad
NickPappagiorgio <nemes1988@icloud.com>
2018-03-07 15:54:58 +0100

    Nice

---
+++ Week4/Day3/Animals_in_the_Zoo.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+abstract class Animal {
+  name: string;
+  age: number;
+  gender: string;
+  numberOfLegs: number;
+  numberOfEyes: number;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+  breed() { }
+}
+
+abstract class Breed extends Animal {
+  breed() {
+    return 'laying eggs.';
+  }
+}
+class Mammal extends Animal {
+  breed() {
+    return 'pushing miniature versions out.';
+  }
+}
+
+class Reptile extends Breed {
+}
+
+class Bird extends Breed {
+}
+
+let reptile = new Reptile("Crocodile");
+let mammal = new Mammal("Koala");
+let bird = new Bird("Parrot");
+
+console.log("How do you breed?");
+console.log("A " + reptile.getName() + " is breeding by " + reptile.breed());
+console.log("A " + mammal.getName() + " is breeding by " + mammal.breed());
+console.log("A " + bird.getName() + " is breeding by " + bird.breed());
