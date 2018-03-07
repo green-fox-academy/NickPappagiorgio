28870ea74252cab495c1584b6b4c9adb2f2d8322
NickPappagiorgio <nemes1988@icloud.com>
2018-03-07 16:59:10 +0100

    I'm totally adding this to epic win. +300

--- Week4/Day3/Comparable.ts
+++ Week4/Day3/Comparable.ts
@@ -1,4 +1,6 @@
-'use strict';
+'use strict'
+
+import { Printable } from './Printable';
 
 interface Comparable {
   compareTo(other: Comparable): number;
@@ -9,13 +11,17 @@ interface Comparable {
   */
 }
 
-class Domino implements Comparable {
+class Domino implements Comparable, Printable {
   values: number[];
   constructor(valueA: number, valueB: number) {
     this.values = [valueA, valueB];
   }
-  compareTo() {
-    if ()
+
+  compareTo(thing: Domino) {
+    return this.values[0] - thing.values[0];
+  }
+  printAllFields() {
+    return this.values;
   }
 }
 
@@ -27,6 +33,49 @@ dominoes.push(new Domino(6, 7));
 dominoes.push(new Domino(2, 4));
 dominoes.push(new Domino(7, 1));
 
-dominoes.sort(function (a: Domino, b: Domino): number {
+let completed: Domino[] = dominoes.sort(function (a: Domino, b: Domino): number {
   return a.compareTo(b);
 });
+
+console.log(completed);
+
+// the order of your dominoes should look like this: [[1,5], [2,4], [4,6], [5,2], [6,7], [7,1]]
+//let descripton: number[] = [1,5, 2,4, 4,6, 5,2, 6,7, 7,1];
+class Thing implements Comparable, Printable {
+  private name: string;
+  private completed: boolean;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+  public complete() {
+    this.completed = true;
+  }
+
+  compareTo(thing: Thing): number {
+    return this.completed === thing.completed ? [this.name, thing.name].sort().indexOf(this.name) - 0.5 : this.completed ? -1 : 1;
+  }
+  printAllFields() {
+    return this.name + this.completed;
+  }
+}
+
+let toDos: Thing[] = [];
+toDos.push(new Thing('Get milk'));
+toDos.push(new Thing('Remove the obstacles'));
+toDos.push(new Thing('Stand up'));
+toDos.push(new Thing('Eat lunch'));
+toDos[2].complete();
+toDos[3].complete();
+
+console.log(toDos.sort(function (a: Thing, b: Thing): number {
+  return a.compareTo(b);
+}));
+
+for (let domino of dominoes) {
+  domino.printAllFields();
+}
+
+for (let todo of toDos) {
+  todo.printAllFields();
+}
