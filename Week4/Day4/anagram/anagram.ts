5a12b7f422339840ae0a4ccaa6c742ff2cf91129
NickPappagiorgio <nemes1988@icloud.com>
2018-03-08 14:58:39 +0100

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

---
+++ Week4/Day4/anagram/anagram.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+export class Anagram {
+  anagram(stringOne: string, stringTwo: string): boolean {
+
+    if (stringOne.length !== stringTwo.length) {
+      return false;
+    }
+
+    let check: number = 0;
+    for (let i: number = 0; i < stringOne.length; i++) {
+      for (let j: number = 0; j < stringTwo.length; j++) {
+        if (stringOne.charAt(i) === stringTwo.charAt(j)) {
+          check++;
+        }
+      }
+    } if (check === stringOne.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+let ddd = new Anagram;
+console.log(ddd.anagram('mozi', 'izom'));
