2ff0bb7d73d45b40d71ed1a72b9018945e9371ce
NickPappagiorgio <nemes1988@icloud.com>
2018-03-12 18:47:28 +0100

    Fucking submodule bull shit

---
+++ Week3/Day2/Doubled/doubled.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a method that decrypts duplicated-chars.txt
+
+const fs = require('fs');
+let fileContent = fs.readFileSync('duplicated_chars.txt', 'utf-8');
+
+function remove(input: string) {
+  return input.split('').map((e, i, a) => {
+    if (e === a[i + 1]) {
+      a.splice(i + 1, 1);
+    } return e;
+  }).join('');
+}
+console.log(remove(fileContent))
