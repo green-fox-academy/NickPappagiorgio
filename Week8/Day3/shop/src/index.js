cfc25d1a03e9763aeda8770871decf28bab5a178
NickPappagiorgio <nemes1988@icloud.com>
2018-04-11 18:57:20 +0200

    Added temp engine

--- Week8/Day3/shop/src/index.js
+++ Week8/Day3/shop/src/index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index', {
+    title: 'Dashboard',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
