294f894dd6dd60ca83d87195da9df8d4891a399a
NickPappagiorgio <nemes1988@icloud.com>
2018-04-17 15:23:20 +0200

    Rora's_Cargo

--- Week9/Day2/iamgroot/routes.js
+++ Week9/Day2/iamgroot/routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+let ship = {
+  caliber25: 0,
+  caliber30: 0,
+  caliber50: 0,
+  shipstatus: 'empty',
+  ready: false
+}
+
 app.get('/groot', (req, res) => {
-  // TODO: implement this method
   if (req.query.message === undefined) {
     res.status(400).json({
       error: 'I am Groot!'
@@ -16,7 +23,6 @@ app.get('/groot', (req, res) => {
 });
 
 app.get('/yondu', (req, res) => {
-  // TODO: implement this method
   if (parseInt(req.query.time) === 0) {
     res.status(400).json({
       error: 'I am Groot!'
@@ -26,6 +32,42 @@ app.get('/yondu', (req, res) => {
       distance: req.query.distance,
       time: req.query.time,
       Speed: req.query.distance / req.query.time
+    })
+  };
+});
+
+app.get('/rocket', (req, res) => {
+  res.json(ship);
+});
+
+app.get('/rocket/fill', (req, res) => {
+  if (req.query.caliber === undefined && req.query.amount === undefined) {
+    res.status(400).json({
+      error: 'I am Groot!'
+    });
+  } else {
+    if (req.query.caliber === '.25') {
+      ship.caliber25 += parseInt(req.query.amount);
+    } else if (req.query.caliber === '.30') {
+      ship.caliber30 += parseInt(req.query.amount);
+    } else if (req.query.caliber === '.50') {
+      ship.caliber50 += parseInt(req.query.amount);
+    };
+
+    let totalAmmo = ship.caliber25 + ship.caliber30 + ship.caliber50;
+    ship.shipstatus = `${((totalAmmo) * 100) / 12500}%`;
+    if (ship.shipstatus === '100%') {
+      ship.shipstatus = 'Full!'
+    } else if (parseInt(ship.shipstatus) > 100) {
+      ship.shipstatus = 'Overloaded!'
+    };
+    ship.ready = parseInt(totalAmmo) === 12500;
+
+    res.json({
+      received: req.query.caliber,
+      amount: req.query.amount,
+      shipstatus: ship.shipstatus,
+      ready: ship.ready,
     })
   };
 });
--- Week9/Day2/iamgroot/test/test-routes.js
+++ Week9/Day2/iamgroot/test/test-routes.js
@@ -42,8 +42,48 @@ test('groot endpoint', (t) => {
       t.error(err);
     });
 
-    request(app)
+  request(app)
     .get('/yondu/?distance=100.0&time=0')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .expect({
+      error: "I am Groot!"
+    })
+    .end((err, res) => {
+      t.error(err);
+    });
+
+  request(app)
+    .get('/rocket')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect({
+      caliber25: 0,
+      caliber30: 0,
+      caliber50: 0,
+      shipstatus: "empty",
+      ready: false
+    })
+    .end((err, res) => {
+      t.error(err);
+    });
+
+  request(app)
+    .get('/rocket/fill/?caliber=.50&amount=12500')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect({
+      received: ".50",
+      amount: "12500",
+      shipstatus: "Full!",
+      ready: true
+    })
+    .end((err, res) => {
+      t.error(err);
+    });
+
+  request(app)
+    .get('/rocket/fill/')
     .expect('Content-Type', /json/)
     .expect(400)
     .expect({
