e34ee6783ead50a97076bed3e62aa240148c8528
NickPappagiorgio <nemes1988@icloud.com>
2018-04-17 11:50:54 +0200

    I_am_groot

---
+++ Week9/Day2/iamgroot/test/test-routes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const app = require('../routes');
+
+test('groot endpoint', (t) => {
+  // TODO: implement it
+  request(app)
+    .get('/groot/?message=Hello!')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect({
+      received: 'Hello!',
+      translated: 'I am Groot!'
+    })
+    .end((err, res) => {
+      t.error(err);
+    });
+
+  request(app)
+    .get('/groot')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .expect({
+      error: 'I am Groot!'
+    })
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
