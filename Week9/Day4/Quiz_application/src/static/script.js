c2f4494f0f07f694b83b2b76123759dc8cbbb438
NickPappagiorgio <nemes1988@icloud.com>
2018-04-19 19:27:15 +0200

    Quiz_fixing

--- Week9/Day4/Quiz_application/src/static/script.js
+++ Week9/Day4/Quiz_application/src/static/script.js
@@ -1,11 +1,14 @@
 const quizDiv = document.querySelector('#quiz');
+const scoreHtml = document.querySelector('span')
+let score = 0;
 
 function getAQuestion() {
   const XHR = new XMLHttpRequest();
   XHR.open('GET', '/game');
   XHR.setRequestHeader('Content-Type', 'application/json');
   XHR.onload = (event) => {
-    //console.log(JSON.parse(event.target.response));
+    const question = document.querySelector('#quiz');
+    question.innerHTML = '';
     createTable(JSON.parse(event.target.response));
   }
   XHR.send();
@@ -17,55 +20,24 @@ function createTable(quiz) {
   p.textContent = quiz.question;
   quizDiv.appendChild(p);
 
-  let buttonOne = nodeCreator('button');
-  buttonOne.className = 'button';
-  buttonOne.innerText = quiz.answers[0].answer;
-  eventListenerMaker(buttonOne);
-  quizDiv.appendChild(buttonOne);
+  quiz.answers.forEach(e => {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = e.answer;
+    quizDiv.appendChild(button);
+  });
 
-  let buttonTwo = nodeCreator('button');
-  buttonTwo.className = 'button';
-  buttonTwo.innerText = quiz.answers[1].answer;
-  eventListenerMaker(buttonTwo);
-  quizDiv.appendChild(buttonTwo);
-
-  let buttonThree = nodeCreator('button');
-  buttonThree.className = 'button';
-  buttonThree.innerText = quiz.answers[2].answer;
-  eventListenerMaker(buttonThree);
-  quizDiv.appendChild(buttonThree);
-
-  let buttonFour = nodeCreator('button');
-  buttonFour.className = 'button';
-  buttonFour.innerText = quiz.answers[3].answer;
-  eventListenerMaker(buttonFour);
-  quizDiv.appendChild(buttonFour);
-}
-
-function eventListenerMaker(button) {
-  button.onclick = (event) => {
-    console.log(event)
-    // let postTarget = 'I Don\'t Know Yet';
-
-    // if (event.target.textContent === "Move Here") {
-    //   postTarget = 'movehere';
-    // } else if (event.target.textContent === '<- To Planet') {
-    //   postTarget = 'toplanet';
-    // } else {
-    //   postTarget = 'toship';
-    // }
-
-    // const XHR = new XMLHttpRequest;
-    // XHR.open('POST', `/${postTarget}/${event.target.parentNode.parentNode.rowIndex}`);
-    // XHR.onload = () => {
-    //   const tbodyRows = document.querySelectorAll('tbody tr');
-    //   tbodyRows.forEach(row => {
-    //     tbody.removeChild(row);
-    //   });
-    //   getPlanets();
-    // }
-    // XHR.send();
-  }
+  let buttons = document.querySelectorAll('.button');
+  scoreHtml.innerText = score;
+  buttons.forEach((e, i) => {
+    e.addEventListener('click', () => {
+      if (quiz.answers[i].is_correct === 1) {
+        score++;
+        scoreHtml.innerText = score;
+        getAQuestion();
+      }
+    });
+  });
 }
 
 function nodeCreator(nodeName) {
